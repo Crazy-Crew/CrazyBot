e4f4705e6fe1c2f37db327467f81633699b02c01
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-27 00:13:34 -0400

    enable about command

--- src/main/java/me/badbones69/crazybot/bot/CrazyBot.java
+++ src/main/java/me/badbones69/crazybot/bot/CrazyBot.java
@@ -1,7 +1,10 @@
 package me.badbones69.crazybot.bot;
 
 import me.badbones69.crazybot.api.discord.VitalDiscord;
+import me.badbones69.crazybot.api.discord.commands.CommandHandler;
 import me.badbones69.crazybot.api.util.LogUtil;
+import me.badbones69.crazybot.bot.commands.AboutCommand;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
@@ -10,13 +13,20 @@ import java.util.List;
 
 public class CrazyBot extends VitalDiscord {
 
+    private final CommandHandler commandHandler;
+
     public CrazyBot(@NotNull final String token, @NotNull final List<GatewayIntent> keys, @NotNull final List<CacheFlag> flags, @NotNull final String folder) {
         super(LogUtil.getLogger(), folder, token, keys, flags);
+
+        this.commandHandler = new CommandHandler(
+                "!",
+                this.jda
+        );
     }
 
     @Override
     public void ready(@NotNull final Guild guild) {
-
+        this.commandHandler.setGuild(guild);
     }
 
     @Override
@@ -26,7 +36,9 @@ public class CrazyBot extends VitalDiscord {
 
     @Override
     public void ready() {
-
+        List.of(
+                new AboutCommand("about", "Shows the information about the Discord Bot.", Permission.MESSAGE_SEND)
+        ).forEach(this.commandHandler::addCommand);
     }
 
     @Override

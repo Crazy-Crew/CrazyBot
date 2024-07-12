c168a0209c2be57b08d3826839afae0138fec1d9
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-12 00:20:20 -0400

    cycle presences randomly every 60 seconds because why not

--- src/main/java/me/badbones69/crazybot/bot/CrazyBot.java
+++ src/main/java/me/badbones69/crazybot/bot/CrazyBot.java
@@ -2,10 +2,16 @@ package me.badbones69.crazybot.bot;
 
 import me.badbones69.crazybot.api.discord.VitalDiscord;
 import me.badbones69.crazybot.bot.commands.AboutCommand;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.jetbrains.annotations.NotNull;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class CrazyBot extends VitalDiscord {
 
@@ -19,5 +25,25 @@ public class CrazyBot extends VitalDiscord {
                 new AboutCommand(this.properties.getProperty("version"))
                 //new SmugCommand()
         ).forEach(this.commandHandler::registerCommand);
+
+        new Timer().scheduleAtFixedRate(
+                new TimerTask() {
+                    @Override
+                    public void run() {
+                        switch (ThreadLocalRandom.current().nextInt(3)) {
+                            case 0 -> jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.customStatus("Type /about to see my stats!"));
+                            case 1 -> jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.customStatus("I can smell you!"));
+                            case 2 -> jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.customStatus("Playing Wizard101"));
+                        }
+                    }
+                },
+                0,
+                60000
+        );
+    }
+
+    @Override
+    public void ready(Guild guild) {
+
     }
 }

68e2fa86a7611046e5908855a7e5192b894e0093
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-12 00:20:09 -0400

    don't extend vital

--- src/main/java/me/badbones69/crazybot/Main.java
+++ src/main/java/me/badbones69/crazybot/Main.java
@@ -1,17 +1,14 @@
 package me.badbones69.crazybot;
 
-import com.ryderbelserion.vital.core.Vital;
 import me.badbones69.crazybot.api.discord.util.files.FileManager;
 import me.badbones69.crazybot.bot.CrazyBot;
 import me.badbones69.crazybot.bot.util.config.ConfigFile;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
-import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.util.List;
-import java.util.logging.Logger;
 
-public class Main extends Vital {
+public class Main {
 
     private static final File dataFolder = new File("vital");
     private static final FileManager fileManager = new FileManager(dataFolder);
@@ -40,20 +37,5 @@ public class Main extends Vital {
 
     public static FileManager getFileManager() {
         return fileManager;
-    }
-
-    @Override
-    public @NotNull File getDirectory() {
-        return dataFolder;
-    }
-
-    @Override
-    public @NotNull Logger getLogger() {
-        return null;
-    }
-
-    @Override
-    public boolean isLogging() {
-        return false;
     }
 }
